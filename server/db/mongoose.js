5764be6768d84f415bb80f8a119cd3f4c6865344
Steven Deferme <sdeferme@quant.be>
2019-02-16 21:32:09 +0100

    Modification mongoose.jq --> remove erronous content

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose
-
 mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}${process.env.MONGOURI}` || 'mongodb://127.0.0.1:27017/TodoApp',{
     useNewUrlParser: true, 
     }, function(error){
