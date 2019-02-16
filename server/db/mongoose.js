ea322cf25132b8bf69e476b104cf7aeb46859247
Steven Deferme <sdeferme@quant.be>
2019-02-16 20:56:46 +0100

    Modification mongoose.jq --> add process.env.MONGOURI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,31 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
 
- /* mongoose.connect('process.env.MONGODB_URI', function() { /* dummy function  })
-    .then(() => {
-        console.log('ok');
-    })
-    .catch(err => { // mongoose connection error will be handled here
-        console.error('App starting error:', err.stack);
-        process.exit(1);
-    });*/
+mongoose
 
-/*    mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`,{
-useNewUrlParser: true, 
-}, function(error){
-    console.log(error);
-    console.log(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`)
-
-});*/
-
-mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`,{
+mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}${process.env.MONGOURI}` || 'mongodb://localhost:27017/TodoApp',{
     useNewUrlParser: true, 
     }, function(error){
         console.log(error);
-        console.log(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`)
-    
     });
 
 module.exports = { mongoose };
