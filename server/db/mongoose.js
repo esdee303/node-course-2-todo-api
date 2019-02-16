69a5c41d14ca61111777b82ce864525a2dcd3912
Steven Deferme <sdeferme@quant.be>
2019-02-16 05:02:29 +0100

    mongoose.js update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,13 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
 
+mongoose.connect('process.env.MONGODB_URI', function() { /* dummy function */ })
+    .then(() => {
+        return server.start();
+    })
+    .catch(err => { // mongoose connection error will be handled here
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
+
 module.exports = { mongoose };
