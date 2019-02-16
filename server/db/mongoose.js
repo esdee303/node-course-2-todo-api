93c280a0c1721e1ca8369480105a0052f9f42614
Steven Deferme <sdeferme@quant.be>
2019-02-16 05:30:02 +0100

    mongoose.js update 7

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -20,11 +20,11 @@ useNewUrlParser: true,
 
 });*/
 
-mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-ftq1f.mongodb.net/test?retryWrites=true`,{
+mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`,{
     useNewUrlParser: true, 
     }, function(error){
         console.log(error);
-        console.log(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@c@cluster0-ftq1f.mongodb.net/test?retryWrites=true`)
+        console.log(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`)
     
     });
 
