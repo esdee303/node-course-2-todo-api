1cac09d5e2f33ce9380e1429314782793067f82f
Steven Deferme <sdeferme@quant.be>
2019-02-16 05:17:05 +0100

    mongoose.js update 5

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -14,6 +14,10 @@ mongoose.Promise = global.Promise;
 
     mongoose.connect(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`,{
 useNewUrlParser: true, 
-}, function(error){console.log(error)});
+}, function(error){
+    console.log(error);
+    console.log(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0-shard-00-00-ftq1f.mongodb.net:27017,cluster0-shard-00-01-ftq1f.mongodb.net:27017,cluster0-shard-00-02-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`)
+
+});
 
 module.exports = { mongoose };
