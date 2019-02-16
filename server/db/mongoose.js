3b80ba469335e3d6ae9cdcf52aeca224b770a2ad
Steven Deferme <sdeferme@quant.be>
2019-02-16 05:14:37 +0100

    mongoose.js update 4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
 
- /* mongoose.connect('process.env.MONGODB_URI', function() { /* dummy function */ })
+ /* mongoose.connect('process.env.MONGODB_URI', function() { /* dummy function  })
     .then(() => {
         console.log('ok');
     })
