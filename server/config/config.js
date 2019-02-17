e1fb3f4176e0ff71c45d46db72bab81a234d1fc4
Steven Deferme <steven.deferme@gmail.com>
2019-02-17 19:15:00 +0100

    Setup separate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if(env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+    process.env.MONGODB_URI = 'mongodb://esdee303:bdog2015@cluster0-shard-00-00-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,11 +8,18 @@ mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp')
 
 
+
+/*
  mongoose.connect(`mongodb://esdee303:bdog2015@cluster0-shard-00-00-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`
   ,{ useNewUrlParser: true, }, function(error){
       console.log(error);
 }) || ('mongodb://localhost:27017/TodoApp');
+*/
 
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, }, function(error) {
+      console.log(error);
+});
 
 
 module.exports = { mongoose };
+
