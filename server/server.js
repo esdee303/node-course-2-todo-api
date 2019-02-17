ed040b4922088d1f7bd52103147e5f8578e35b41
Steven Deferme <sdeferme@quant.be>
2019-02-17 17:08:30 +0100

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,9 +57,10 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo) {
             return res.status(404).send();
         }
-        res.send(todo);
-}).catch((e) => {
-    res.status(400).send();
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 if(!module.parent){
