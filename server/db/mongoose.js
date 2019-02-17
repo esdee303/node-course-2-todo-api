e14a8019d53cfe542a600dcc3b8816d8d3c19a2d
Steven Deferme <steven.deferme@gmail.com>
2019-02-17 17:59:32 +0100

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,10 +8,10 @@ mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp')
 
 
- mongoose.connect(`mongodb://esdee303:bdog2015@cluster0-shard-00-00-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true` || 'mongodb://localhost:27017/TodoApp'
+ mongoose.connect(`mongodb://esdee303:bdog2015@cluster0-shard-00-00-ftq1f.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`
   ,{ useNewUrlParser: true, }, function(error){
       console.log(error);
-});
+}) || ('mongodb://localhost:27017/TodoApp');
 
 
 
