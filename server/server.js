b74a52791962b1fdb95f880a618a9fa11b3b75c8
Steven Deferme <steven.deferme@gmail.com>
2019-02-18 23:12:24 +0100

    Testing PST /user/login -- Add test for POST /users/login

--- server/server.js
+++ server/server.js
@@ -112,7 +112,7 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-// POST /users/login {email, password}
+// POST /users/login
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     
